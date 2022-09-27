c0366deaf846bdd8c556c70cf94e87acbaa09b7b
dongwook <dongwook@localhost>
2022-09-27 10:46:55 +0900

    Spring 2-Layered Architecture

---
+++ test7/src/main/java/com/jang/biz/board/BoardVO.java
@@ -0,0 +1,53 @@
+package com.jang.biz.board;
+
+public class BoardVO {
+	private int bid;
+	private String title;
+	private String writer;
+	private String content;
+	private int cnt;
+	private String regdate;
+	
+	public int getBid() {
+		return bid;
+	}
+	public void setBid(int bid) {
+		this.bid = bid;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getCnt() {
+		return cnt;
+	}
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+	public String getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+	
+	@Override
+	public String toString() {
+		return "BoardVO [bid=" + bid + ", title=" + title + ", writer=" + writer + ", content=" + content + ", cnt="
+				+ cnt + ", regdate=" + regdate + "]";
+	}
+}
